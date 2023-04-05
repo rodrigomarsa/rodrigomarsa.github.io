bee26f739b357a181884c8c33ec625356dd66470
Rodrigo <rodrigomartins.agro@gmail.com>
2023-04-04 22:08:17 -0300

    feat: add about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../styles/About.css';
 
 function About() {
   return (
     <div className="About">
       <h1>Sobre mim</h1>
+      <h2>
+        Sou engenheiro agrônomo apaixonado por tecnologia, após muito tempo dedicado na
+        área de agronomia, estou em transição de carreira para o desenvolvimento web
+        através do curso da
+        {' '}
+        <a href="https://www.betrybe.com/">Trybe</a>
+        , aonde aprendi muito sobre ReactJS em front-end e NodeJS em back-end, além de
+        várias outras hard-skills e soft-skills, bem como metodologias agéis.
+        Nas horas vagas adoro ficar com minha família e praticar esportes.
+      </h2>
     </div>
   );
 }
